0c42b0abb94b6e4101a4a6b2de83f8fe1755ec4e
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-13 21:56:43 -0700

    Enrollment finds by name

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -3,16 +3,28 @@ require "pry"
 require_relative "enrollment"
 
 class EnrollmentRepository
-  attr_accessor :enrollment
+  attr_accessor :enrollments
 
   def initialize
+    @enrollments = []
   end
 
   def load_data(data)
-    self.enrollment = Enrollment.new(name: "enrollment")
+    collection = Hash.new
+    CSV.foreach(data[:enrollment][:kindergarten],
+                headers: true, header_converters: :symbol) do |row|
+      collection[row[:location]] = Hash.new
+      collection[row[:location]][row[:timeframe]] = row[:data]
+    end
+    collection.map do |location, data|
+      self.enrollments << Enrollment.new(name: location,
+                                         kindergarten_participation: data)
+    end
   end
 
   def find_by_name(name)
-    enrollment
+    self.enrollments.find do |element|
+      element.name == name
+    end
   end
 end
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -25,6 +25,18 @@ class EnrollmentRepositoryTest < Minitest::Test
     })
     enrollment = er.find_by_name("ACADEMY 20")
 
-    assert_equal(er.find_by_name("ACADEMY 20").object_id, enrollment.object_id)      
+    assert_equal(er.find_by_name("ACADEMY 20").object_id, enrollment.object_id)
+  end
+
+  def test_it_finds_by_name
+    er = EnrollmentRepository.new
+    er.load_data({
+      enrollment: {
+        kindergarten: "./fixtures/Kindergartners in full-day program.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+
+    assert_equal("ACADEMY 20", enrollment.name)
   end
 end
