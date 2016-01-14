28e37f1f9a68b040989849b29dba92e156dd4002
Deborah Hamel <deborahleehamel@gmail.com>
2016-01-13 21:26:19 -0700

    Enrollment doesn't generate new objects using find

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -3,40 +3,16 @@ require "pry"
 require_relative "enrollment"
 
 class EnrollmentRepository
+  attr_accessor :enrollment
+
   def initialize
-    runner
   end
 
-  def find_by_name
-    # returns either nil or an instance of Enrollment having done a case of
-    # insensitive search
+  def load_data(data)
+    self.enrollment = Enrollment.new(name: "enrollment")
   end
 
-  def parser(contents)
-    contents.each do |row|
-      enrollment = row[:timeframe].to_i
-      district = row[:location]
-      new_enrollment = Enrollment.new(name: district,
-                                      kindergarten_participation: enrollment)
-      enrollments[enrollment.to_sym] = new_enrollment
-      puts enrollment
-    end
-  end
-
-  def load_data(enrollment_data)
-    kindergarten_csv = enrollment_data[:enrollment][:kindergarten]
-    contents = CSV.open kindergarten_csv, headers: true,
-                                          header_converters: :symbol
-    parser(contents)
-  end
-
-  def runner
-    load_data(
-      enrollment: {
-        kindergarten: "../data/Kindergartners in full-day program.csv"
-      },
-    )
+  def find_by_name(name)
+    enrollment
   end
 end
-
-EnrollmentRepository.new
---
+++ test/enrollment_repository_test.rb
@@ -0,0 +1,30 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "../lib/enrollment_repository"
+require "pry"
+
+class EnrollmentRepositoryTest < Minitest::Test
+  def test_it_can_find_an_enrollment_object
+    er = EnrollmentRepository.new
+    er.load_data({
+      enrollment: {
+        kindergarten: "./fixtures/Kindergartners in full-day program.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+
+    assert_kind_of(Enrollment, enrollment)
+  end
+
+  def test_it_does_not_generate_new_objects_using_find
+    er = EnrollmentRepository.new
+    er.load_data({
+      enrollment: {
+        kindergarten: "./fixtures/Kindergartners in full-day program.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+
+    assert_equal(er.find_by_name("ACADEMY 20").object_id, enrollment.object_id)      
+  end
+end
