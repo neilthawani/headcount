338cfe9b8f5584e76e010ff0e60a6e8d08421788
neilthawani@gmail.com <neilthawani@gmail.com>
2016-08-06 13:41:44 -0500

    Added teardown

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -24,46 +24,56 @@ class HeadCountAnalystTest < Minitest::Test
     headcount_analyst = HeadcountAnalyst.new(district_repository)
   end
 
+  def setup
+    @district_repository = district_repository
+    @headcount_analyst = headcount_analyst
+  end
+
+  def teardown
+    @district_repository = nil
+    @headcount_analyst = nil
+  end
+
   def test_head_analyst_exists
-    assert headcount_analyst
+    assert @headcount_analyst
   end
 
   def test_does_head_analyst_init_with_district_repo
-    assert_kind_of DistrictRepository, headcount_analyst.district_repository
+    assert_kind_of DistrictRepository, @headcount_analyst.district_repository
   end
 
   def test_if_kidergarten_participation_avg_compares_to_state_average
-    assert_equal 0.766, headcount_analyst.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
+    assert_equal 0.766, @headcount_analyst.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
   end
 
   def test_if_kidergarten_participation_compares_to_another_district
-    assert_equal 0.573, headcount_analyst.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'ADAMS COUNTY 14')
+    assert_equal 0.573, @headcount_analyst.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'ADAMS COUNTY 14')
   end
 
   def test_if_kidergarten_participation_rate_trends_against_state_average
     district_trend = {2007=>0.992, 2006=>1.051, 2005=>0.96, 2004=>1.258, 2008=>0.718, 2009=>0.652, 2010=>0.681, 2011=>0.728, 2012=>0.688, 2013=>0.694, 2014=>0.661}
 
-    assert_equal district_trend, headcount_analyst.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
+    assert_equal district_trend, @headcount_analyst.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
   end
 
   def test_if_hs_graduation_avg_compares_to_state_average
-    assert_equal 1.194, headcount_analyst.high_school_graduation_variation("ACADEMY 20", :against => 'COLORADO')
+    assert_equal 1.194, @headcount_analyst.high_school_graduation_variation("ACADEMY 20", :against => 'COLORADO')
   end
 
   def test_does_kindergarten_participation_affect_hs_graduation
-    assert_equal 0.642, headcount_analyst.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
+    assert_equal 0.642, @headcount_analyst.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
   end
 
   def test_does_kinder_participation_correlate
-    assert_equal true, headcount_analyst.kindergarten_participation_correlates_with_high_school_graduation(for: 'ACADEMY 20')
+    assert_equal true, @headcount_analyst.kindergarten_participation_correlates_with_high_school_graduation(for: 'ACADEMY 20')
   end
 
   def test_do_statewide_percentages_correlate_with_grad_rates
-    assert_equal true, headcount_analyst.kindergarten_participation_correlates_with_high_school_graduation(for: 'STATEWIDE')
+    assert_equal true, @headcount_analyst.kindergarten_participation_correlates_with_high_school_graduation(for: 'STATEWIDE')
   end
 
   def test_does_kinder_participation_correlate_with_a_subset_of_districts
     districts = ['ACADEMY 20','ADAMS COUNTY 14', 'ADAMS-ARAPAHOE 28J', 'AGUILAR REORGANIZED 6']
-    assert_equal true, headcount_analyst.kindergarten_participation_correlates_with_high_school_graduation(:across => districts)
+    assert_equal true, @headcount_analyst.kindergarten_participation_correlates_with_high_school_graduation(:across => districts)
   end
 end
