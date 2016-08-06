b147605129233bba51b4b3a4930d3af690058f2b
neilthawani@gmail.com <neilthawani@gmail.com>
2016-08-06 13:22:04 -0500

    Renamed variable so it's clearer

--- test/district_test.rb
+++ test/district_test.rb
@@ -4,7 +4,7 @@ require "district"
 
 class DistrictTest < Minitest::Test
   def test_can_we_pass_in_a_name
-    d = District.new(name: "ACADEMY 20")
-    assert_equal "ACADEMY 20", d.name
+    district = District.new(name: "ACADEMY 20")
+    assert_equal "ACADEMY 20", district.name
   end
 end
