6465831dd114e233d6301fb0d55b18036e7bff81
neilthawani@gmail.com <neilthawani@gmail.com>
2016-08-06 15:23:20 -0500

    Added explicit setup/teardown

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -18,7 +18,7 @@ class EnrollmentRepositoryTest < Minitest::Test
     { :high_school_graduation_rates => path }
   end
 
-  def enrollment_repository
+  def setup
     file_paths = [kindergarten_participation_access_hash, hs_graduation_rate_access_hash]
 
     @enrollment_repository ||= begin
@@ -28,18 +28,22 @@ class EnrollmentRepositoryTest < Minitest::Test
     end
   end
 
+  def teardown
+    @enrollment_repository = nil
+  end
+
   def test_it_can_find_an_enrollment_object
-    assert_kind_of Enrollment, enrollment_repository.find_by_name("ACADEMY 20")
+    assert_kind_of Enrollment, @enrollment_repository.find_by_name("ACADEMY 20")
   end
 
   def test_it_does_not_generate_new_objects_using_find
-    enrollment = enrollment_repository.find_by_name("ACADEMY 20")
+    enrollment = @enrollment_repository.find_by_name("ACADEMY 20")
 
-    assert_equal(enrollment_repository.find_by_name("ACADEMY 20").object_id, enrollment.object_id)
+    assert_equal(@enrollment_repository.find_by_name("ACADEMY 20").object_id, enrollment.object_id)
   end
 
   def test_it_finds_by_name
-    enrollment = enrollment_repository.find_by_name("ACADEMY 20")
+    enrollment = @enrollment_repository.find_by_name("ACADEMY 20")
 
     assert_equal("ACADEMY 20", enrollment.name)
   end
@@ -51,38 +55,38 @@ class EnrollmentRepositoryTest < Minitest::Test
                    2010 => 0.436, 2011 => 0.489, 2012 => 0.478,
                    2013 => 0.487, 2014 => 0.490 }
 
-    enrollment = enrollment_repository.find_by_name("ACADEMY 20")
+    enrollment = @enrollment_repository.find_by_name("ACADEMY 20")
 
     assert_equal(expected_kindergarten_participation, enrollment.kindergarten_participation)
   end
 
   def test_find_by_name_is_not_case_sensitive
-    enrollment = enrollment_repository.find_by_name("coloRADo")
+    enrollment = @enrollment_repository.find_by_name("coloRADo")
 
     assert_equal("Colorado", enrollment.name)
   end
 
   def test_find_by_name_returns_nil_for_no_input
-    enrollment = enrollment_repository.find_by_name(nil)
+    enrollment = @enrollment_repository.find_by_name(nil)
 
     assert_equal(nil, enrollment)
   end
 
   def test_find_by_name_returns_nil_with_random_special_characters
-    enrollment = enrollment_repository.find_by_name("Colo*!^*@#rado")
+    enrollment = @enrollment_repository.find_by_name("Colo*!^*@#rado")
 
     assert_equal(nil, enrollment)
   end
 
   def test_graduation_rate_by_year
     expected_grad_rate = {2010=>0.57, 2011=>0.608, 2012=>0.633, 2013=>0.593, 2014=>0.659}
-    enrollment = enrollment_repository.find_by_name("ADAMS COUNTY 14")
+    enrollment = @enrollment_repository.find_by_name("ADAMS COUNTY 14")
 
     assert_equal expected_grad_rate, enrollment.high_school_graduation_rates
   end
 
   def test_graduation_rate_in_year
-    enrollment = enrollment_repository.find_by_name("ADAMS COUNTY 14")
+    enrollment = @enrollment_repository.find_by_name("ADAMS COUNTY 14")
     
     assert_equal 0.57, enrollment.high_school_graduation_rates[2010]
   end
