bb6b947062400443b62e063ffbca240e09a3ac15
neilthawani@gmail.com <neilthawani@gmail.com>
2016-08-06 15:26:48 -0500

    Added explicit setup/teardown

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -3,7 +3,7 @@ require "minitest/pride"
 require "enrollment"
 
 class EnrollmentTest < Minitest::Test
-  def enrollment
+  def setup
     @enrollment ||= begin
       enrollment = Enrollment.new(name: "ACADEMY 20",
                                   kindergarten_participation: {
@@ -14,29 +14,31 @@ class EnrollmentTest < Minitest::Test
     end
   end
 
-  def test_we_can_create_an_instance
-    kindergarten_participation = {2010 => 0.3915,
-                                  2011 => 0.35356,
-                                  2012 => 0.1234}
-                                  
-    assert_equal "ACADEMY 20", enrollment.name
+  def teardown
+    @enrollment = nil
+  end
 
-    assert_equal(kindergarten_participation, enrollment.kindergarten_participation)
+  def expected_kinder_participation_data
+    {2010 => 0.3915,
+      2011 => 0.35356,
+      2012 => 0.1234}
+  end
+
+  def test_we_can_create_an_instance
+    assert_equal "ACADEMY 20", @enrollment.name
+
+    assert_equal(expected_kinder_participation_data, @enrollment.kindergarten_participation)
   end
 
   def test_it_can_get_kindergarten_participation_by_year
-    kindergarten_participation = {2010 => 0.3915,
-                                  2011 => 0.35356,
-                                  2012 => 0.1234}
-
-    assert_equal(kindergarten_participation, enrollment.kindergarten_participation)
+    assert_equal(expected_kinder_participation_data, @enrollment.kindergarten_participation)
   end
 
   def test_it_can_get_kindergarten_participation_by_year_by_year
-    assert_equal(0.3915, enrollment.kindergarten_participation[2010])
+    assert_equal(0.3915, @enrollment.kindergarten_participation[2010])
   end
 
   def test_it_returns_nil_for_participation_by_year_by_year_that_does_not_exist
-    assert_equal(nil, enrollment.kindergarten_participation[2009])
+    assert_equal(nil, @enrollment.kindergarten_participation[2009])
   end
 end
